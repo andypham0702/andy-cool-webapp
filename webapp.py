654edae68bb3b88645796493aa3da62a1ed434ef
Andy Pham <aqpham02@gmail.com>
2021-05-05 18:41:54 -0400

    Update

--- webapp.py
+++ webapp.py
@@ -14,23 +14,31 @@ For all your Streamlit questions, check out their [documentation](https://docs.s
 
 
 '''
-## Plot COVID-19 Cases visualization :mask: :mask: :mask:
+## I. Plot a bar chart with COVID-19 Cases dataset :mask: :mask: :mask:
 '''
 
-#Covid Cases Visualization 
-covid = pd.read_csv('https://raw.githubusercontent.com/shinokada/covid-19-stats/master/data/daily-new-confirmed-cases-of-covid-19-tests-per-case.csv')
+# Covid Cases Visualization 
+covid = pd.read_csv('data/confirmed_covid19_cases.csv')
+
+# You can use this direct link to load the csv file as well: "https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbTFoQ0lHcGpBR2dVTEtjQTdWOGpaakRnUHM1d3xBQ3Jtc0ttQjVPMi15aFhPWWZaMHI2c1lHNnZ4NWU1Qk1zVG1LU3JjTTN3TkhBMTdMVDZNSWFNNTNBWWtGalVtY0R1QjFPRko5TmVwQ2FaeUhoR3JuX1ZSNjBSeDBjNUJzTUdVVE5scExTcGFTS3d1czRuUHpXNA&q=https%3A%2F%2Fraw.githubusercontent.com%2Fshinokada%2Fcovid-19-stats%2Fmaster%2Fdata%2Fdaily-new-confirmed-cases-of-covid-19-tests-per-case.csv"
+
+# Change the column names
 covid.columns = ['Country', 'Code', 'Date', 'Confirmed', 'Days since confirmed']
+
+# Change the date format
 covid['Date'] = pd.to_datetime(covid['Date']).dt.strftime('%Y-%m-%d')
 
+# Show the covid dataframe in Streamlit
 st.write(covid)
 
+# Create unique values in the column "Country" and "Date"
 country_options = covid['Country'].unique().tolist()
 date_options = covid['Date'].unique().tolist()
 
-# Select box for date
+# Create select box for date
 date = st.selectbox('Which date would you like to see?', date_options, 100)
 
-# Select box for country
+# Create select box for country
 country = st.multiselect('Which country would you like to see?', country_options, ['Brazil'])
 
 # Assign new values for variable 'covid' for visualization
@@ -41,6 +49,7 @@ covid = covid[covid['Country'].isin(country)]
 fig2 = px.bar(covid, x= 'Country', y= 'Confirmed', color= 'Country', title='Global COVID-19 Cases',
 				range_y=[0, 35000], animation_frame='Date', animation_group='Country')
 
+# Adjust the speed of the graph
 fig2.layout.updatemenus[0].buttons[0].args[1]['frame']['duration'] = 30
 fig2.layout.updatemenus[0].buttons[0].args[1]['transition']['duration'] = 5
 
@@ -51,11 +60,12 @@ st.write(fig2)
 
 
 '''
-## Building predictive models
+## II. Building predictive models :mag: :mag: :mag:
 '''
 
 
 @st.cache(allow_output_mutation=True)
+
 def load_model(path):
     model = joblib.load(path)
     return model
